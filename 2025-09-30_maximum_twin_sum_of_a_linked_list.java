187d90e8522f4a15fefdf6181633a7b99229746f
siddhaviraul22 <raulsiddhavi678@gmail.com>
2025-09-30 19:26:47 +0530

    Leetcode- Maximum Twin Sum of a Linked List Solution

---
+++ 2025-09-30_maximum_twin_sum_of_a_linked_list.java
@@ -0,0 +1,53 @@
+// Problem: Odd Even Linked List
+// Link:https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/?envType=study-plan-v2&envId=leetcode-75
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        // Step 1: Find the middle of the list
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse the second half of the list
+        ListNode secondHalf = reverse(slow);
+
+        // Step 3: Calculate max twin sum
+        int maxSum = 0;
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            maxSum = Math.max(maxSum, firstHalf.val + secondHalf.val);
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return maxSum;
+    }
+
+    // Helper to reverse a linked list
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+}
