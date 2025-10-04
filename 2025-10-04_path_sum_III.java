fca9b2dedd796bf4221581b27c829e80a65673cc
siddhaviraul22 <raulsiddhavi678@gmail.com>
2025-10-04 23:48:49 +0530

    Leetcode- Path Sum III Solution

---
+++ 2025-10-04_path_sum_III.java
@@ -0,0 +1,41 @@
+// Problem: Path Sum III
+// Link:https://leetcode.com/problems/path-sum-iii/description/?envType=study-plan-v2&envId=leetcode-75
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1); // Base case: sum = 0 exists once
+        return dfs(root, 0L, targetSum, prefix);
+    }
+
+    private int dfs(TreeNode node, long currSum, int target, Map<Long, Integer> prefix) {
+        if (node == null) return 0;
+
+        currSum += node.val;
+        int count = prefix.getOrDefault(currSum - target, 0);
+
+        prefix.put(currSum, prefix.getOrDefault(currSum, 0) + 1);
+
+        count += dfs(node.left, currSum, target, prefix);
+        count += dfs(node.right, currSum, target, prefix);
+
+        prefix.put(currSum, prefix.get(currSum) - 1); // Backtrack
+
+        return count;
+    }
+}
