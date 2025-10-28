a0f6788d1c4a51119c4f6d12b72043c33594ac5d
siddhaviraul22 <raulsiddhavi678@gmail.com>
2025-10-28 15:47:15 +0530

    Leetcode- Combination Sum III Solution

---
+++ 2025-10-26_letter_combination_sum_iii.java
@@ -0,0 +1,27 @@
+// Problem: Combination Sum III
+// Link: https://leetcode.com/problems/combination-sum-iii/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), k, n, 1);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int k, int remain, int start) {
+        if (current.size() == k && remain == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (current.size() >= k || remain < 0) {
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            current.add(i);
+            backtrack(result, current, k, remain - i, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
