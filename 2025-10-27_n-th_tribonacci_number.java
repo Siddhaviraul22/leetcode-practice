c4a79410618bf035b15d04614ac1d6bc83367bb9
siddhaviraul22 <raulsiddhavi678@gmail.com>
2025-10-28 16:05:19 +0530

    Leetcode- N-th Tribonacci Number Solution

---
+++ 2025-10-27_n-th_tribonacci_number.java
@@ -0,0 +1,18 @@
+// Problem: N-th Tribonacci Number
+// Link: https://leetcode.com/problems/n-th-tribonacci-number/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            int next = a + b + c;
+            a = b;
+            b = c;
+            c = next;
+        }
+        return c;
+    }
+}
