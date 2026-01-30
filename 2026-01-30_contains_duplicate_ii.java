d6143e86e7c009818ddc3fe33843ff905544b2f7
siddhaviraul22 <raulsiddhavi678@gmail.com>
2026-01-30 23:19:24 +0530

    Leetcode-

---
+++ 2026-01-30_contains_duplicate_ii.java
@@ -0,0 +1,19 @@
+// Problem: Longest Consecutive Sequence
+// Link: https://leetcode.com/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> lastIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (lastIndex.containsKey(nums[i])) {
+                if (i - lastIndex.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            lastIndex.put(nums[i], i);
+        }
+        return false;
+    }
+}
+
